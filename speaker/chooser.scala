762879f0fd97819ac4b273c46bcbb48bf2d00964
numa08 <n511287@gmail.com>
2013-04-14 02:59:05 +0900

    スピーカー選択ワンライナー

---
+++ speaker/chooser.scala
@@ -0,0 +1,1 @@
+scala.util.Random.shuffle("alstamber" :: "masawada" ::"U_Akihir0" :: "KHe7" ::"eserver_dip_jp" :: "numa08" ::"murasaki8823" :: "catatsuy" ::"shitsyndrom" :: "k5khrS"  ::"bilyakudan" :: "tmyt"	 ::"kembo_org" :: "oidong1" ::"dora22sibuya" :: "yosida95" ::"gu_pen" :: "kazoo04" :: Nil).foreach(println(_))
